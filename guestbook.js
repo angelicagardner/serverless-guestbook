fb405c62ef32728b47b075d369893d549f521c92
Angelica Gardner <23157778+angelicagardner@users.noreply.github.com>
2022-07-23 14:28:04 +0200

    update API route url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://5b475b41.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
